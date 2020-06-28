0b4e8c70841abdcdfb6a1dde83dbcec41275e14b
hyunjusong <hun3780@gmail.com>
2020-06-28 18:09:11 +0900

    eddit regard encode

--- server/request-handler.js
+++ server/request-handler.js
@@ -8,6 +8,7 @@ requestHandler 함수를 export 하여 basic-server.js 에서 사용 할 수 있
 
 **************************************************************/
 let message = [];
+//한글로 데이터 입력할때, 데이터 깨지고 업데이트도 느려짐
 const requestHandler = function(request, response) {
   request.setEncoding('utf-8')
   // 기본 CORS 설정이 되어있는 코드 입니다. 아래에 있습니다.
@@ -15,8 +16,6 @@ const requestHandler = function(request, response) {
   let headers = defaultCorsHeaders;
   // 응답 헤더에 응답하는 컨텐츠의 자료 타입을 헤더에 기록 합니다.
   headers["Content-Type"] = "text/plain";
-  // .writeHead() 메소드는 응답 헤더에 해당 key, value 를 적어줍니다.
-  // response.writeHead(statusCode, headers);
   // 노드 서버에 대한 모든 요청은 응답이 있어야 합니다. response.end 메소드는 요청에 대한 응답을 보내줍니다.
   request.on('data', (chunk) => {
     message.push(JSON.parse(chunk));
@@ -41,6 +40,16 @@ const requestHandler = function(request, response) {
     response.writeHead(404, headers);
     response.end("method error");
   }
+};
+
+const defaultCorsHeaders = {
+  "access-control-allow-origin": "*",
+  "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
+  "access-control-allow-headers": "content-type, accept",
+  "access-control-max-age": 10 // Seconds.
+};
+
+module.exports = requestHandler;
   /*
   request.on('end', () => {
     if (request.method === "POST") {
@@ -66,7 +75,6 @@ const requestHandler = function(request, response) {
   //console.log(
   //  `http request method is ${request.method}, url is ${request.url}`
   //);
-};
 
 // These headers will allow Cross-Origin Resource Sharing (CORS).
 // This code allows this server to talk to websites that
@@ -77,11 +85,3 @@ const requestHandler = function(request, response) {
 //
 // Another way to get around this restriction is to serve you chat
 // client from this domain by setting up static file serving.
-const defaultCorsHeaders = {
-  "access-control-allow-origin": "*",
-  "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
-  "access-control-allow-headers": "content-type, accept",
-  "access-control-max-age": 10 // Seconds.
-};
-
-module.exports = requestHandler;
